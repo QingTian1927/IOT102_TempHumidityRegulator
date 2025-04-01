f20c4f964b62c78e6271ca0a27ca0275ccc84dfa
QingTian1927 <120935290+QingTian1927@users.noreply.github.com>
2025-04-01 16:12:19 +0700

    feat: statistics API route.

--- webserver/src/main/java/com/example/iotwebserver/controller/ApiController.java
+++ webserver/src/main/java/com/example/iotwebserver/controller/ApiController.java
@@ -6,9 +6,16 @@ import com.example.iotwebserver.service.ReadingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("api")
+@RequestMapping("/api")
 public class ApiController {
+
+    public static final String STATISTICS_HOURLY = "hourly";
+    public static final String STATISTICS_DAILY = "daily";
+    public static final String STATISTICS_WEEKLY = "weekly";
 
     private final ReadingService readingService;
 
@@ -21,5 +28,23 @@ public class ApiController {
     public void uploadReading(@RequestBody ReadingUpload readingUpload) {
         Reading reading = readingUpload.toReading();
         this.readingService.save(reading);
+    }
+
+    @GetMapping("/statistics/{type}")
+    public List<Object[]> getStatistics(@PathVariable String type) {
+        switch (type) {
+            case STATISTICS_HOURLY -> {
+                return readingService.getHourlyAverages();
+            }
+            case STATISTICS_DAILY -> {
+                return readingService.getDailyAveragesForWeek();
+            }
+            case STATISTICS_WEEKLY -> {
+                return readingService.getWeeklyAveragesForMonth();
+            }
+            default -> {
+                return new ArrayList<>();
+            }
+        }
     }
 }
